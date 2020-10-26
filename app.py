b15680200f655fa3d65a1964ce6247fc05786b4e
Nitin Faye <47500844+nitinfaye@users.noreply.github.com>
2020-10-27 02:20:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -24,66 +24,60 @@ from PIL import Image
 #app=Flask(__name__)
 #Swagger(app)
 
-pickle_in = open("model_scores.pkl","rb")
+pickle_in = open("model_scores.p","rb")
 classifier=pickle.load(pickle_in)
+
+Model_List=['LINEAR REGRASSION', 'RANDOM FOREST','LSTM','ARIMA','PROPHET','HOLT-WINTER']
+
 
 #@app.route('/')
 def welcome():
     return "Welcome All"
+	
+def user_input_features():
 
-#@app.route('/predict',methods=["Get"])
-def predict_covid_cases(Confirmed,Deaths,Recovred):
+    st.sidebar.header('User Input Values')
+
+    From_Date = st.sidebar.slider('From Date March', 1  , 31, 10)
+    To_Date = st.sidebar.slider('To Date May', 1 , 31, 10)
+	
+	   ##st.sidebar.add_rows
+
+    Country = st.sidebar.text_input('Please inter Country',)
+
+       #st.sidebar.add_rows
+
+    Model = st.sidebar.selectbox('Select Model',Model_List)
+
+    data = {'From_Date': From_Date,	
+            'To_Date': To_Date,	
+            'Country': Country,
+			'Model':  Model}
+    features = pd.DataFrame(data, index=[0])
+    return features
+		
     
-    """Let's Authenticate the Banks Note 
-    This is using docstrings for specifications.
-    ---
-    parameters:  
-      - name: variance
-        in: query
-        type: number
-        required: true
-      - name: skewness
-        in: query
-        type: number
-        required: true
-      - name: curtosis
-        in: query
-        type: number
-        required: true
-      - name: entropy
-        in: query
-        type: number
-        required: true
-    responses:
-        200:
-            description: The output values
-        
-    """
-   
-    prediction=classifier.predict([[Confirmed,Deaths,Recovred]])
-    print(prediction)
-    return prediction
+df = user_input_features()
 
+st.subheader('User Entered parameters for confirmed cases, Deaths cases and Recovred cases is')
 
+st.write(df)
 
 def main():
     st.title("Covid Analysis")
+	
+	
+	
     html_temp = """
     <div style="background-color:tomato;padding:10px">
     <h2 style="color:white;text-align:center;">Streamlit Covid Analyser ML App </h2>
     </div>
     """
     st.markdown(html_temp,unsafe_allow_html=True)
-    Confirmed = st.text_input("Confirmed","Type Here")
-    Deaths = st.text_input("Deaths","Type Here")
-    Recovred = st.text_input("Recovred","Type Here")
-    result=""
-    if st.button("Predict"):
-        result=predict_covid_cases(Confirmed,Deaths,Recovred)
-    st.success('The output is {}'.format(result))
-    if st.button("About"):
-        st.text("Lets LEarn")
-        st.text("Built with Streamlit")
+	
+    
+    st.text("Lets LEarn")
+    st.text("Built with Streamlit")
 
 if __name__=='__main__':
     main()
